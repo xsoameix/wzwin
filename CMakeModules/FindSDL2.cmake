b1ae9b118d3b0a468b0b37d8c580c0eece3bd949
Lien Chiang <xsoameix@gmail.com>
2016-04-22 09:33:46 +0800

    Support Windows

--- CMakeModules/FindSDL2.cmake
+++ CMakeModules/FindSDL2.cmake
@@ -13,6 +13,28 @@
 #  BSD license.
 
 if ("${CMAKE_C_COMPILER_ID}" STREQUAL "MSVC")
+  set(_SDL2_ROOT_HINTS ${SDL2_ROOT_DIR} ENV SDL2_ROOT_DIR)
+  find_path(SDL2_INCLUDE_DIR
+    NAMES SDL2/SDL.h
+    HINTS ${_SDL2_ROOT_HINTS}
+    PATH_SUFFIXES "include")
+  find_library(_SDL2_LIBRARY
+    NAMES SDL2
+    HINTS ${_SDL2_ROOT_HINTS}
+    PATH_SUFFIXES "lib")
+  find_library(_SDL2_MAIN_LIBRARY
+    NAMES SDL2main
+    HINTS ${_SDL2_ROOT_HINTS}
+    PATH_SUFFIXES "lib")
+  set(_SDL2_CORE_LIBRARY user32 gdi32 winmm imm32 ole32 oleaut32 version uuid)
+  set(_SDL2_JOYSTICK_LIBRARY dinput8 dxguid)
+  set(SDL2_LIBRARIES
+    ${_SDL2_LIBRARY}
+    ${_SDL2_MAIN_LIBRARY}
+    ${_SDL2_CORE_LIBRARY}
+    ${_SDL2_JOYSTICK_LIBRARY})
+  find_package_handle_standard_args(SDL2 "Could NOT find SDL2, try to set the path to SDL2 root folder in the system variable SDL2_ROOT_DIR"
+    SDL2_LIBRARIES SDL2_INCLUDE_DIR)
 else()
   find_package(PkgConfig QUIET REQUIRED)
   pkg_search_module(SDL2 REQUIRED QUIET sdl2)
